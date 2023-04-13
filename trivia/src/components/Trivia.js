1af60da2bde830304f438da0558199509bdb7a61
yash-d99 <dubey.yash2@gmail.com>
2023-04-12 22:13:01 -0400

    final working copy of trivia game

--- trivia/src/components/Trivia.js
+++ trivia/src/components/Trivia.js
@@ -1,25 +1,45 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import Button from '@mui/material/Button';
 function Trivia (props) {
     const myProp = props.name;
     console.log(myProp);
-    const [color, setColor] = useState("secondary");
-    const click = color => {
-        setColor(color);
+    // let i =0;
+    // let array = [];
+    // myProp.map((question) => (
+    //    array.push(question)
+    // ));
+    // console.log(array);
+    const [rightColor, setRightColor] = useState("secondary");
+    const [wrongColor, setWrongColor] = useState("secondary");
+    const [correctColor, setCorrectColor] = useState("secondary");
+    const [rightAnswer, setRightAnswer] = useState(true);
+    const rightClick = color => {
+        setRightColor(color);
+        setRightAnswer(true);
     }
-    // useEffect(() => {
-    //     document.body.style.backgroundColor = color;
-    // }, [color])
+    const wrongClick = color => {
+        setWrongColor(color);
+        setRightAnswer(false);
+    }
+    
     return (
         <>
+            {rightAnswer ? (
+            <Button variant = "contained" color = {rightColor}>Correct</Button> 
+                ):
+            (<Button variant = "contained" color = {wrongColor}>Wrong</Button>)
+            }            
             {myProp.map((question) => (
             <>
                 <p>{question.question}?</p>
-                <Button variant = "contained" color = "secondary">{question.correct_answer}</Button>
+                <Button variant = "contained" onClick = {
+                        () => rightClick("success") 
+                    }
+                >{question.correct_answer}</Button>
                 
                 {question.incorrect_answers.map((wrong_answer) => (
-                    <Button variant = "contained" color = {color} onClick = {
-                        () => click("error")
+                    <Button variant = "contained" onClick = {
+                        () => wrongClick("error")
                     }
                     >
                     {wrong_answer}</Button>
